5c9e57b4919530dffd23c53851e27fc13c03c9f2
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-05-22 03:17:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 #Assigning labels to minikube node
-minikube start --driver=docker
+minikube start --driver=docker --cpus 8 --memory 11894
 kubectl label node minikube nodeApp1=ldap
 kubectl label node minikube nodeApp2=eos-mgm
 kubectl label node minikube nodeApp3=eos-fstN
 kubectl label node minikube nodeApp4=cernbox
 kubectl label node minikube nodeApp5=cernbox
 kubectl label node minikube nodeApp6=cernboxgateway
-kubectl label node minikube nodeApp7=swan
+kubectl label node minikube nodeApp7=swan 
 kubectl label node minikube nodeApp8=swan-users
 #Creation of persistant volumes
-sudo mkdir -p /mnt/ldap/userdb
-sudo mkdir -p /mnt/ldap/config
-sudo mkdir -p /mnt/cbox_shares_db/cbox_data
-sudo mkdir -p /mnt/cbox_shares_db/cbox_MySQL
-sudo chmod -rwx '/mnt'
-gnome-terminal -- minikube mount /mnt:/mnt
+mkdir -p $HOME/mnt/ldap/userdb
+mkdir -p $HOME/mnt/ldap/config
+mkdir -p $HOME/mnt/cbox_shares_db/cbox_data
+mkdir -p $HOME/mnt/cbox_shares_db/cbox_MySQL
+gnome-terminal -- minikube mount $HOME/mnt:/mnt
 #Deployement of Services
 kubectl apply -f BOXED.yaml
 kubectl apply -f LDAP.yaml
