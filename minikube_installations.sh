ba3d23b5efe48064eecb5ab63cce397c73d9712d
Enrico Bocchi <enrico.bocchi@cern.ch>
2020-05-26 19:46:28 +0200

    Rename unused files

--- minikube_installations.sh
+++
@@ -1,4 +0,0 @@
-#install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
-mkdir -p /usr/local/bin/
-install minikube /usr/local/bin/
