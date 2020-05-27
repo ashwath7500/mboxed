93ab43ee7497cd665afa6131c7020e71452e54bd
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-05-27 19:00:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,31 @@
 #Assigning labels to minikube node
-minikube start --driver=docker --cpus 8 --memory 11894
-kubectl label node minikube nodeApp1=ldap
-kubectl label node minikube nodeApp2=eos-mgm
-kubectl label node minikube nodeApp3=eos-fstN
-kubectl label node minikube nodeApp4=cernbox
-kubectl label node minikube nodeApp5=cernbox
-kubectl label node minikube nodeApp6=cernboxgateway
-kubectl label node minikube nodeApp7=swan 
-kubectl label node minikube nodeApp8=swan-users
+sudo minikube start --driver=none
+NODE_NAME=$(sudo kubectl get nodes | grep master | cut -d ' ' -f 1)
+sudo kubectl label node $NODE_NAME nodeApp1=ldap
+sudo kubectl label node $NODE_NAME nodeApp2=eos-mgm
+sudo kubectl label node $NODE_NAME nodeApp3=eos-fst1
+sudo kubectl label node $NODE_NAME nodeApp4=cernbox
+sudo kubectl label node $NODE_NAME nodeApp5=cernbox
+sudo kubectl label node $NODE_NAME nodeApp6=cernboxgateway
+sudo kubectl label node $NODE_NAME nodeApp7=swan 
+sudo kubectl label node $NODE_NAME nodeApp8=swan-users
+sudo kubectl label node $NODE_NAME nodeApp9=eos-mq
 #Creation of persistant volumes
-mkdir -p $HOME/mnt/ldap/userdb
-mkdir -p $HOME/mnt/ldap/config
-mkdir -p $HOME/mnt/cbox_shares_db/cbox_data
-mkdir -p $HOME/mnt/cbox_shares_db/cbox_MySQL
-gnome-terminal -- minikube mount $HOME/mnt:/mnt
+sudo mkdir -p /mnt/ldap/userdb
+sudo mkdir -p /mnt/ldap/config
+sudo mkdir -p /mnt/cbox_shares_db/cbox_data
+sudo mkdir -p /mnt/cbox_shares_db/cbox_MySQL
+sudo mkdir -p /mnt/eos_namespace
+sudo mkdir -p /mnt/fst_userdata
+sudo chmod -rwx '/mnt'
 #Deployement of Services
-kubectl apply -f BOXED.yaml
-kubectl apply -f LDAP.yaml
+sudo kubectl apply -f BOXED.yaml
+sudo kubectl apply -f LDAP.yaml
+sudo kubectl apply -f eos-storage-mgm.yaml
+sudo kubectl apply -f eos-storage-fst1.yaml
+sudo kubectl apply -f CERNBOX.yaml
+
+#Execution of necessary commands
+LDAP_PODNAME=$(sudo kubectl -n boxed get pods -o wide | grep ldap* | cut -d ' ' -f 1)
+sudo kubectl exec -n BOXED $LDAP_PODNAME -- bash /root/addusers.sh
+ 
