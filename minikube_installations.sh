49ec5685887b74a677fb01d73d678fe13037dc50
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-06-04 19:21:33 +0530

    Update minikube_installations.sh

--- minikube_installations.sh
+++ minikube_installations.sh
@@ -17,45 +17,53 @@ need_root
 
 # Install basic software
 echo "Installing basic software..."
-apt-get install -y install \
+apt-get install -y \
   curl \
   git
 
+# Install dependencies
+echo "Installing dependencies..."
+apt-get install -y \
+  conntrack
 
 # Install minikube
 #   Docs at https://kubernetes.io/docs/tasks/tools/install-minikube/
 echo "Installing minikube..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version --client
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube
 mkdir -p /usr/local/bin/
-install minikube /usr/local/bin/
-rm -f minikube
+curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube
+chmod +x /usr/local/bin/minikube
+minikube version
 
+# Install kubectl
+#   Docs at https://kubernetes.io/docs/tasks/tools/install-kubectl/
+echo "Installing kubectl..."
+mkdir -p /usr/local/bin/
+curl -L https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+chmod +x /usr/local/bin/kubectl
+kubectl version
 
 # Install docker
 #   Docs at https://docs.docker.com/engine/install/ubuntu/
 # TODO: We should consider the ubuntu version
 # TODO: Make docker version configurable
 echo "Installing docker..."
-sudo apt-get update
-sudo apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update
+apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io
 
 # Start docker
 echo "Starting docker daemon..."
@@ -64,5 +72,3 @@ if [ "$?" -ne 0 ]; then
   systemctl start docker
 fi
 docker --version
-
-sudo apt-get install -y conntrack
